7a5211dbb17d054f3b5319d9754f29b05eb5bb9c
Amayakite <amayakite@qq.com>
2021-11-01 20:43:46 +0800

    add NavBarLink

--- blog/.vuepress/navBar.js
+++ blog/.vuepress/navBar.js
@@ -6,7 +6,13 @@ module.exports = [
         icon: "note",
         items: [
             { text: "VuePressTheme-Hope", icon: "vue", link: "https://vuepress-theme-hope.github.io/zh/" },
-            {text:"JDK1.8手册",icon:'java',link:"https://www.matools.com/api/java8"},
+            {
+                text: "Java常用网站", icon: 'java',
+                items: [
+                    { text: "JDK1.8手册", icon: 'note', link: "https://www.matools.com/api/java8" },
+                    { text: "HuTool工具类库", icon: 'plugin', link: "https://www.hutool.cn/" }
+                ]
+            },
         ]
     },
 ]
